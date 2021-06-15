3ee0b5f19f21a75b43900350594c67303e73cb7a
Luke Ballew <lxb190013@sysbio.utdallas.edu>
2021-06-14 23:31:06 -0500

    add assignment 3 files

---
+++ submit_cov.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=featurecounts
+#SBATCH --ntasks=8
+#SBATCH --nodes=1
+#SBATCH --mem=8G
+#SBATCH --partition=normal
+#SBATCH --time=30:00
+#SBATCH --mail-user=lxb190013@utdallas.edu
+#SBATCH --mail-type=ALL
+
+ml load subread
+featureCounts -t gene -g gene_id -a cov_genome.gtf -o counts.txt test.paired_end.sorted.bam
